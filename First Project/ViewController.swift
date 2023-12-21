0d868528170b81401dfddce749d06666a0d6236d
martin0330 <martin.cruz0330@yahoo.com>
2023-12-20 16:59:53 -0800

    Initial Commit

---
+++ First Project/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  First Project
+//
+//  Created by Martin cruz on 12/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+    
+        func changeColor() -> UIColor{
+            let red = CGFloat.random(in: 0...1)
+            let green = CGFloat.random(in: 0...1)
+            let blue = CGFloat.random(in: 0...1)
+            
+            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+        }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
